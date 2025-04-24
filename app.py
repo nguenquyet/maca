fffb84445880915854fbf70f0fb00da9facfb314
nguenquyet <132254704+nguenquyet@users.noreply.github.com>
2025-04-24 21:53:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "API Sự Kiện Hôm Nay hoạt động!"
+    return "Xin chào từ Flask! Đây là API cà phê Sự Kiện Hôm Nay!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
